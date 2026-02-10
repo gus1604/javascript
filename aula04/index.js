dfcfd60ce21a2d8b9866e819a5a6f1e74c8f412b
gus1604 <gustavosilva1604200@gmail.com>
2026-02-10 10:00:05 -0300

    Update index.js

--- aula04/index.js
+++ aula04/index.js
@@ -1,3 +1,9 @@
-window.alert('Minha primeira mensagem!')
-window.confirm('Está gostando de JavaScript.')
-window.prompt('Qual é seu nome?')
+window.alert('Minha primeira mensagem!')//Cria um alerta
+window.confirm('Está gostando de JavaScript.')//Janela com o botão ok e cancelar.
+window.prompt('Qual é seu nome?') //Vai perquntar seu nome.
+
+var n1 = 200
+var n2 = 15
+var n3 =8.5
+
+var s1 = "JavaScript"
