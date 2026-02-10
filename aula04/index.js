b93032db46eac562606959f71ad9c07c4c3b6fba
gus1604 <gustavosilva1604200@gmail.com>
2026-02-10 13:13:57 -0300

    aula06

--- aula04/index.js
+++ aula04/index.js
@@ -2,8 +2,10 @@ window.alert('Minha primeira mensagem!')//Cria um alerta
 window.confirm('Está gostando de JavaScript.')//Janela com o botão ok e cancelar.
 window.prompt('Qual é seu nome?') //Vai perquntar seu nome.
 
+//Ex de variaveis
 var n1 = 200
 var n2 = 15
 var n3 =8.5
 
 var s1 = "JavaScript"
+
