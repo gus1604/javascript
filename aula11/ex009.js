b0db85279f7210ea776c226b5ec6bdfe22fcf48b
gus1604 <gustavosilva1604200@gmail.com>
2026-02-22 13:06:16 -0300

    Ex011

--- aula11/ex009.js
+++ aula11/ex009.js
@@ -1,6 +1,6 @@
-var pais = 'França'
+var pais = 'Brasil'
 console.log(`Vivendo em ${pais}`)
-if (pais != 'Brasil'){
+if (pais.toLowerCase() != 'brasil'){
     console.log('Você é Estrangeiro!')
 } else{
     console.log('Você é Brasileiro!')
